928ec57af39a8690fa957bac4de6ea480eea045f
Andrew Li <54134293+andrewleaf22@users.noreply.github.com>
2022-12-26 01:36:50 -0600

    code: backend alpha 0.1

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import conn from "../db/conn";
 import dotenv from "dotenv";
+import { ObjectId } from "mongodb";
 
 type Judge = {
     name: string,
@@ -20,7 +21,8 @@ type Evaluation = {
     citation: number,
     coverage: number,
     bias: number,
-    weight: number
+    weight: number,
+    date: Date
 }
 
 dotenv.config();
@@ -28,28 +30,44 @@ const router = express.Router();
 const dbo = conn;
 
 // if it works
-router.route("/test/").get((_: Request, res: Response) => {
-    res.json({response: 403, message: "No API key"});
+router.route("/test").get((_: Request, res: Response) => {
+    res.json({status: "No API key"});
+});
+
+// auth
+// key -> boolean
+router.route("/auth/:key").get((req: Request, res: Response) => {
+    if(req.params.key == process.env.PASSWORD) {
+        res.json({status: "Correct Password", auth: true});
+    } else {
+        res.json({status: "Incorrect Password", auth: false});
+    }
 });
 
 // get one judge
 // key, string -> Judge
-router.route("/get/judge/:apikey/:judgeName").get((req: Request, res: Response) => {
+router.route("/get/judge/:apikey/:judgeId").get((req: Request, res: Response) => {
     const dbConnect = dbo.getDb();
     if(!req.params.apikey) {
-        res.json({response: 403, message: "No API key"});
+        res.json({status: "No API key"});
+    } else if (!req.params.judgeId) {
+        res.json({status: "No judge given"});
     } else {
         if(req.params.apikey!=process.env.APIKEY) {
-            res.json({response: 403, message: "Incorrect API key"});
+            res.json({status: "Incorrect API key"});
         } else {
             dbConnect
             .collection("judges")
-            .findOne({name: req.params.judgeName}, (err: Error, result: Response) => {
+            .findOne({_id: new ObjectId(req.params.judgeId)}, (err: Error, result: Response) => {
                 if (err) throw err;
-                res.json(result);
+                res.json({result: result, status: `Found judge ${req.params.judgeId}`});
             });
         }
     }
+});
+
+router.route("/get/judge/:apikey/").get((req: Request, res: Response) => {
+    res.json({status: "No judge given"});
 });
 
 // get all judges
@@ -57,17 +75,17 @@ router.route("/get/judge/:apikey/:judgeName").get((req: Request, res: Response) 
 router.route("/get/alljudges/:apikey").get((req: Request, res: Response) => {
     const dbConnect = dbo.getDb();
     if(!req.params.apikey) {
-        res.json({response: 403, message: "No API key"});
+        res.json({status: "No API key"});
     } else {
         if(req.params.apikey!=process.env.APIKEY) {
-            res.json({response: 403, message: "Incorrect API key"});
+            res.json({status: "Incorrect API key"});
         } else {
             dbConnect
             .collection("judges")
             .find({})
             .toArray(function (err: Error, result: Response) {
                 if (err) throw err;
-                res.json(result);
+                res.json({result: result, status: `Found all judges`});
             });
         }
     }
@@ -78,10 +96,10 @@ router.route("/get/alljudges/:apikey").get((req: Request, res: Response) => {
 router.route("/create/judge").post(async (req: Request, res: Response) => {
     const dbConnect = dbo.getDb();
     if(!req.body.apikey) {
-        res.json({response: 403, message: "No API key"});
+        res.json({status: "No API key"});
     } else {
         if(req.body.apikey!=process.env.APIKEY) {
-            res.json({response: 403, message: "Incorrect API key"});
+            res.json({status: "Incorrect API key"});
         } else {
             const judge: Judge = {
                 name: req.body.name,
@@ -93,7 +111,7 @@ router.route("/create/judge").post(async (req: Request, res: Response) => {
         
             dbConnect.collection("judges").insertOne(judge, (err: Error, resp: Response) => {
                 if (err) throw err;
-                res.json(resp);
+                res.json({result: resp, status: `Created judge ${judge.name}`});
             });
         }
     }
@@ -103,13 +121,13 @@ router.route("/create/judge").post(async (req: Request, res: Response) => {
 // Judge UUID -> void
 router.route("/update/judge/:apikey/:judgeid").post(async (req: Request, res: Response) => {
     const dbConnect = dbo.getDb();
-    if(!req.body.apikey) {
-        res.json({response: 403, message: "No API key"});
+    if(!req.params.apikey) {
+        res.json({status: "No API key"});
     } else {
-        if(req.body.apikey!=process.env.APIKEY) {
-            res.json({response: 403, message: "Incorrect API key"});
+        if(req.params.apikey!=process.env.APIKEY) {
+            res.json({status: "Incorrect API key"});
         } else {
-            let query = {_id: req.query.judgeid};
+            let query = {_id: new ObjectId(req.params.judgeid)};
             let newEvaluation: Evaluation = {
                 tournamentName: req.body.tName,
                 roundName: req.body.rName, // e.g., Round 1 Flight A etc.
@@ -120,7 +138,8 @@ router.route("/update/judge/:apikey/:judgeid").post(async (req: Request, res: Re
                 citation: req.body.citation,
                 coverage: req.body.coverage,
                 bias: req.body.bias,
-                weight: req.body.isImprovement ? req.body.weight*0.25 : req.body.weight
+                weight: req.body.weight, // needs to be fixed
+                date: new Date()
             };
 
             dbConnect
@@ -128,14 +147,15 @@ router.route("/update/judge/:apikey/:judgeid").post(async (req: Request, res: Re
             .findOne(query, (err: Error, result: any) => {
                 if (err) throw err;
                 // i have that judge now
+                console.log(result);
                 let judgeEvalsCurrent = result.evaluations;
                 judgeEvalsCurrent.push(newEvaluation);
 
                 dbConnect
                 .collection("judges")
-                .updateOne(query, {$set: {evaluations: judgeEvalsCurrent}}, function (err: Error, resp: Response) {
+                .updateOne(query, {$set: {evaluations: judgeEvalsCurrent, totalEarnedPoints: result.totalEarnedPoints+(req.body.decision+req.body.comparison+req.body.citation+req.body.coverage+req.body.bias), totalPossiblePoints: result.totalPossiblePoints+5}}, (err: Error, resp: Response) => {
                     if (err) throw err;
-                    res.json(resp);
+                    res.json({result: resp, status: `Updated judge ${req.params.judgeid}`});
                 });
             });
         }
